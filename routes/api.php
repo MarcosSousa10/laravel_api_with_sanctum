9627b927cf20e28fc5fa49c8af033155de1e7f2e
Marcos <marcospegodesousa10@gmail.com>
2024-10-04 13:30:26 -0300

    correção error

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/status', function () {
+    return response()->json(
+        [
+            'status' => 'ok',
+            'menssage' => 'Api is running'
+        ],
+        200
+    );
+});
