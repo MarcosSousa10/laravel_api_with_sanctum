249acdcaea4e04aacce0f1358e9304d8801414ae
Marcos <marcospegodesousa10@gmail.com>
2024-10-11 10:10:37 -0300

    correção

--- app/Mail/NewUserConfirmation.php
+++ app/Mail/NewUserConfirmation.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class newEmail extends Mailable
+class NewUserConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
